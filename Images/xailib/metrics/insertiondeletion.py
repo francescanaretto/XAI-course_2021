c186388aa63de784795320b415f4129a398c8459
francescobodria <francesco.bodria@sns.it>
2022-01-21 17:21:20 +0100

    Added interactivity on the metrics

--- Images/xailib/metrics/insertiondeletion.py
+++ Images/xailib/metrics/insertiondeletion.py
@@ -4,6 +4,8 @@ import matplotlib.pyplot as plt
 import pandas as pd
 import tensorflow as tf
 import torch
+import matplotlib.animation as animation
+from matplotlib import rc
 
 class ImageInsDel():
     def __init__(self, predict, mode, step, substrate_fn):
@@ -31,8 +33,7 @@ class ImageInsDel():
             rgb (bool): if the image is rgb or grayscale
             verbose (int): in [0, 1, 2].
                 0 - return list of scores.
-                1 - also plot final step.
-                2 - also plot every step and print 2 top classes.
+                1 - plot every step and print 2 top classes.
             save_to (str): directory to save every step plots to.
 
         Return:
@@ -50,52 +51,69 @@ class ImageInsDel():
 
         if self.mode == 'del':
             title = 'Deletion metric'
-            ylabel = 'Pixels deleted'
+            xlabel = 'Pixels deleted'
             start = torch.tensor(img).clone()
             finish = self.substrate_fn(torch.tensor(img))
         elif self.mode == 'ins':
             title = 'Insertion metric'
-            ylabel = 'Pixels inserted'
+            xlabel = 'Pixels inserted'
             start = self.substrate_fn(torch.tensor(img))
             finish = torch.tensor(img).clone()
 
         scores = np.empty(n_steps + 1)
         # Coordinates of pixels in order of decreasing saliency
         salient_order = np.flip(np.argsort(explanation.reshape(-1, HW), axis=1), axis=-1)
-        for i in range(n_steps+1):
-            pred = torch.tensor(self.predict(start.numpy()))
-            pr, cl = torch.topk(pred, 2)
-            if verbose == 2:
-                print('class {}: probability {:.3f}'.format(cl[0][0], float(pr[0][0])))
-                print('class {}: probability {:.3f}'.format(cl[0][1], float(pr[0][1])))
-            scores[i] = pred[0, c]
-            # Render image if verbose, if it's the last step or if save is required.
-            if verbose == 2 or (verbose == 1 and i == n_steps) or save_to:
-                plt.figure(figsize=(10, 5))
-                plt.subplot(121)
-                plt.title('{} {:.1f}%, P={:.4f}'.format(ylabel, 100 * i / n_steps, scores[i]))
-                plt.axis('off')
-                #tensor_imshow(start[0])
+
+        if verbose:
+            fig, ax = plt.subplots(1,2,figsize=(10,5),facecolor='white')
+            
+            #ax[0].axis('off')
+            ax[1].set_title(title)
+            ax[1].set_xlabel(xlabel)
+            ax[1].set_xlim(-0.1, 1.1)
+            ax[1].set_ylim(0, 1.05)
+            line = ax[1].plot([],[],'-o')[0]
+            title = ax[0].set_title('')
+            probs = ax[0].set_xlabel('')
+            title.set_animated(True)
+            probs.set_animated(True)
+            plt.close()
+
+            def init():
+                line.set_data([], [])
+                title.set_text('')
+                probs.set_text('')
+                return line, title, probs
+
+            def animate(i):
+                pred = torch.tensor(self.predict(start.numpy()))
+                pr, cl = torch.topk(pred, 2)
+                probs.set_text(f'class {cl[0][0]} prob {float(pr[0][0]):.3f} \n class {cl[0][1]} prob {float(pr[0][1]):.3f}')
+                scores[i] = pred[0, c]
+                title.set_text('{} {:.1f}%, P={:.4f}'.format(xlabel, 100 * i / n_steps, scores[i]))
+                line.set_data(np.arange(i+1) / n_steps, scores[:i+1])
                 image = (start[0].detach().cpu().numpy()).astype(int)
                 if rgb:
-                    plt.imshow(np.stack([image[0,:,:],image[1,:,:],image[2,:,:]],axis=-1))
+                    ax[0].imshow(np.stack([image[0,:,:],image[1,:,:],image[2,:,:]],axis=-1))
                 else:
-                    plt.imshow(image, cmap='gray')
-
-                plt.subplot(122)
-                plt.plot(np.arange(i+1) / n_steps, scores[:i+1])
-                plt.xlim(-0.1, 1.1)
-                plt.ylim(0, 1.05)
-                plt.fill_between(np.arange(i+1) / n_steps, 0, scores[:i+1], alpha=0.4)
-                plt.title(title)
-                plt.xlabel(ylabel)
-                #plt.ylabel(get_class_name(c))
-                if save_to:
-                    plt.savefig(save_to + '/{:03d}.png'.format(i))
-                    plt.close()
-                else:
-                    plt.show()
-            if i < n_steps:
+                    ax[0].imshow(image, cmap='gray')
                 coords = salient_order[:, self.step * i:self.step * (i + 1)]
                 start.cpu().numpy().reshape(1, CH, HW)[0, :, coords] = finish.cpu().numpy().reshape(1, CH, HW)[0, :, coords]
-        return scores
+                return line, title, probs
+            
+
+            anim = animation.FuncAnimation(fig, animate, init_func=init,
+                                frames=n_steps+1, interval=70, blit=True, repeat=False)
+
+            rc('animation', html='jshtml')
+            return [scores, anim]
+
+        else:
+
+            for i in range(n_steps+1):
+                pred = torch.tensor(self.predict(start.numpy()))
+                scores[i] = pred[0, c]
+                if i < n_steps:
+                    coords = salient_order[:, self.step * i:self.step * (i + 1)]
+                    start.cpu().numpy().reshape(1, CH, HW)[0, :, coords] = finish.cpu().numpy().reshape(1, CH, HW)[0, :, coords]
+            return scores
